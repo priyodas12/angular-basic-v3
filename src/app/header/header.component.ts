1c78e432a06611f8083ec8c4a0d166996b511e4b
Priyobrato.Das <priyodas12@gmail.com>
2023-11-01 20:33:49 +0530

    basic structure

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-header',
-    template: "<h3>E-Kart</h3>"
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
 
