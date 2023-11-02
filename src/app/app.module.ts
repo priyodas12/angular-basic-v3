c33f4333d3e584d165539f8aba81f8ff18fa5fc5
Priyobrato.Das <priyodas12@gmail.com>
2023-11-02 21:14:21 +0530

    search bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMemuComponent } from './header/main-memu/main-memu.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,HeaderComponent, TopHeaderComponent, TopMenuComponent, MainMemuComponent
+    AppComponent,HeaderComponent, TopHeaderComponent, TopMenuComponent, MainMemuComponent, SearchBarComponent
   ],
   imports: [
     BrowserModule,
