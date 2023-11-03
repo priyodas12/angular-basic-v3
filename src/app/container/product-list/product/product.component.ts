e95621991812154ccf3b3d49c97067ca423b3b5e
Priyobrato.Das <priyodas12@gmail.com>
2023-11-03 18:00:48 +0530

    added comment

--- src/app/container/product-list/product/product.component.ts
+++ src/app/container/product-list/product/product.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductComponent {
 
-  @Input()
+  @Input()// consumer from parent component
   product: {
     id: number,
     title: string,
