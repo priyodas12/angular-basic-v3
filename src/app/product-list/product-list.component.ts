5699378fa50aa825e8b373d40c276317ab8b5285
Priyobrato.Das <priyodas12@gmail.com>
2023-11-03 09:25:18 +0530

    attr binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,12 +9,18 @@ import {
 })
 export class ProductListComponent {
 
+  searchUrl = "http://www.google.com";
+  name = "test";
   product = {
     imageUrl: '../../assets/iphone.jpeg',
     name: "Iphone 15",
     price: 12000,
     color: "Black",
     discount: 16,
-    available: false,
+    available: true,
+  }
+
+  onNameChange() {
+    this.name = "john"
   }
 }
